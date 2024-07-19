80ea180c923ab75370870cd4e824ecc19245e733
gitandrew2023 <155172701+gitandrew2023@users.noreply.github.com>
2024-07-19 12:28:13 -0400

    Create main.py
    First

---
+++ main.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+
+
+class Equation(ABC):
+    degree: int
+    
+    def __init__(self, *args):
+        if (self.degree + 1) != len(args):
+            raise TypeError(
+                f"'{self.__class__.__name__}' object takes {self.degree + 1} positional arguments but {len(args)} were given"
+            )
+
+        any(args)
+        for arg in args:
+            if not isinstance(arg, (int, float)):
+                raise TypeError("Coefficients must be of type 'int' or 'float'")
+    def __init_subclass__(cls):
+        if not hasattr(cls, "degree"):
+            raise AttributeError(
+                f"Cannot create '{cls.__name__}' class: missing required attribute 'degree'"
+            )
+    
+    @abstractmethod
+    def solve(self):
+        pass
+        
+    @abstractmethod
+    def analyze(self):
+        pass
+        
+class LinearEquation(Equation):
+    degree = 1
+    
+    def solve(self):
+        pass
+    
+    def analyze(self):
+        pass
+    
+lin_eq = LinearEquation(2, 3)
